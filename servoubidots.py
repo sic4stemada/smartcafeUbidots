71dcc2fe46a253e23251cc5adf07369fe4b96160
sic4stemada <138538752+sic4stemada@users.noreply.github.com>
2023-09-05 18:05:27 +0700

    Update servoubidots.py

--- servoubidots.py
+++ servoubidots.py
@@ -1,47 +1,72 @@
-import RPi.GPIO as GPIO
+from __future__ import division
 import time
+import Adafruit_PCA9685
 import requests
 
-servoPIN = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+# Inisialisasi PCA9685 menggunakan alamat default (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+# Atur frekuensi PWM ke 60Hz (baik untuk servos).
+pwm.set_pwm_freq(60)
 
 TOKEN = "BBFF-dsPG7gaq7lQdbxAJaYApcnIxbTEVHh"
 DEVICE_LABEL = "motodc"
-VARIABLE_LABEL = "servo1"  # Variabel tombol yang sudah Anda buat
+VARIABLE_LABEL = "meja1"
 
 def get_button_status():
-    url = "https://industrial.api.ubidots.com/api/v1.6/devices/motodc/servo1/"
+    url = "https://industrial.api.ubidots.com/api/v1.6/devices/motodc/meja1/"
     headers = {"X-Auth-Token": TOKEN}
     response = requests.get(url, headers=headers)
     data = response.json()
     return data["last_value"]["value"]
 
+# Configure min dan max servo pulse lengths
+servo_min = 150  # Panjang pulsa minimum (0 derajat)
+servo_max = 600  # Panjang pulsa maksimum (180 derajat)
+
+# Menghitung panjang pulsa untuk 60 derajat
+target_degrees = 60
+target_pulse = int(servo_min + (target_degrees / 180.0 * (servo_max - servo_min)))
+
+print('Moving servo on channel 0...')
 try:
-  while True:
+    while True:
         button_status = get_button_status()
 
         print("Button Status:", button_status)
         
         if button_status == 1:
-            print("masuk")
-            p.ChangeDutyCycle(5)
-            time.sleep(2)
-            p.ChangeDutyCycle(8)
-            time.sleep(2)
-            p.ChangeDutyCycle(5)
-            time.sleep(2)
-           
+            time.sleep(4)
+            pwm.set_pwm(12, 0, target_pulse)
+            time.sleep(1)
+            pwm.set_pwm(12, 0, servo_min)
+            time.sleep(1)
+            
+            time.sleep(6)
+            pwm.set_pwm(8, 0, target_pulse)
+            time.sleep(1)
+            pwm.set_pwm(8, 0, servo_min)
+            time.sleep(1)
+            
+            time.sleep(9)
+            pwm.set_pwm(4, 0, target_pulse)
+            time.sleep(1)
+            pwm.set_pwm(4, 0, servo_min)
+            time.sleep(1)
+            
+            time.sleep(12)
+            pwm.set_pwm(2, 0, target_pulse)
+            time.sleep(1)
+            pwm.set_pwm(2, 0, servo_min)
+            time.sleep(1)
 
+            
         elif button_status == 0:
-            print("stop")
-            p.ChangeDutyCycle(5)
-            time.sleep(2)
-   
-   
+            pwm.set_pwm(0, 0, servo_min)
+            time.sleep(1)
 except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+    # Tangani jika pengguna menekan Ctrl-C
+    pwm.set_pwm(0, 0, 0)  # Pastikan servo dimatikan sebelum keluar
+
+# Bersihkan GPIO saat selesai
+pwm.set_pwm(0, 0, 0)
